5693712e2a5f00e72bf782f9ea701125360e2d92
AbdoKhattab11 <mr.spykhattab@gmail.com>
2023-12-21 00:12:55 +0200

    Added js front-end

--- injex.js
+++ injex.js
@@ -1,22 +1,22 @@
-import sql from 'mssql/msnodesqlv8.js';
-import express from 'express';
-import bodyParser from 'body-parser';
+import sql from "mssql/msnodesqlv8.js";
+import express from "express";
+import bodyParser from "body-parser";
 
 const port = 8000;
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const pool = new sql.ConnectionPool({
-  database: 'master',
-  server: 'DESKTOP-UT15SN3\\MSSQLSERVER01',
-  driver: 'msnodesqlv8',
+  database: "ProjectManagement",
+  server: "DESKTOP-OTMR5M4",
+  driver: "msnodesqlv8",
   options: {
-    trustedConnection: true
-  }
-})
+    trustedConnection: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -27,17 +27,16 @@ app.post("/result", (req, res) => {
   pool.connect().then(() => {
     //simple query
     pool.request().query(`select * from ${req.body.fetch}`, (err, result) => {
-          console.dir(result)
-          let list_result = result["recordset"];
-          res.json({
-            keys: Object.keys(list_result[0]),
-            list : list_result
-          });
-      })
-  })
-  
-})
+      console.dir(result);
+      let list_result = result["recordset"];
+      res.json({
+        keys: Object.keys(list_result[0]),
+        list: list_result,
+      });
+    });
+  });
+});
 
 app.listen(port, (req, res) => {
   console.log(`server is running on port ${port}`);
-})
+});
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,21 @@
-
 async function fetch_table_data(table_name) {
-  const table_data = await (await fetch('/result', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({fetch: table_name})
-  })).json()
-  return table_data
+  const table_data = await (
+    await fetch("/result", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fetch: table_name }),
+    })
+  ).json();
+  return table_data;
 }
 
 const defaultTableDisplay = document.querySelector(
   ".default__table__container"
 );
 const allTables = document.querySelectorAll(".table__container");
+
 const emplyeesTableBtn = document.getElementById("emplyees-a");
 const projectsTableBtn = document.getElementById("projects-a");
 const teamsTableBtn = document.getElementById("teams-a");
@@ -29,25 +31,74 @@ const removeExistTable = () => {
 };
 
 emplyeesTableBtn.addEventListener("click", async () => {
-  console.log(await fetch_table_data("Authors"));
+  const obj = await fetch_table_data("Employee");
+  const list = obj["list"];
+  const emplyeeTable = allTables[0].querySelector("table tbody");
+  let html = ``;
+  for (let i = 0; i < list.length; i++) {
+    let newHtml = ``;
+    for (const j in list[i]) {
+      newHtml += `<td>${list[i][j]}</td>`;
+    }
+
+    html += `<tr>${newHtml} <td><button class="del-btn"><i class="fa-solid fa-trash trash-icon"></i></button></td></tr>`;
+  }
+  emplyeeTable.innerHTML = html;
   removeExistTable();
   allTables[0].classList.add("active__table");
 });
 projectsTableBtn.addEventListener("click", async () => {
-  console.log(await fetch_table_data("Books"));
+  const obj = await fetch_table_data("Project");
+  const list = obj["list"];
+  const emplyeeTable = allTables[1].querySelector("table tbody");
+  let html = ``;
+  for (let i = 0; i < list.length; i++) {
+    let newHtml = ``;
+    for (const j in list[i]) {
+      newHtml += `<td>${list[i][j]}</td>`;
+    }
+
+    html += `<tr>${newHtml} <td><button class="del-btn"><i class="fa-solid fa-trash trash-icon"></i></button></td></tr>`;
+  }
+  emplyeeTable.innerHTML = html;
   removeExistTable();
   allTables[1].classList.add("active__table");
 });
 teamsTableBtn.addEventListener("click", async () => {
-  console.log(await fetch_table_data("Writes"));
+  const obj = await fetch_table_data("Team");
+  const list = obj["list"];
+  const emplyeeTable = allTables[2].querySelector("table tbody");
+  let html = ``;
+  for (let i = 0; i < list.length; i++) {
+    let newHtml = ``;
+    for (const j in list[i]) {
+      newHtml += `<td>${list[i][j]}</td>`;
+    }
+
+    html += `<tr>${newHtml} <td><button class="del-btn"><i class="fa-solid fa-trash trash-icon"></i></button></td></tr>`;
+  }
+  emplyeeTable.innerHTML = html;
   removeExistTable();
   allTables[2].classList.add("active__table");
 });
-tasksTableBtn.addEventListener("click", () => {
+tasksTableBtn.addEventListener("click", async () => {
+  const obj = await fetch_table_data("Task");
+  const list = obj["list"];
+  const emplyeeTable = allTables[3].querySelector("table tbody");
+  let html = ``;
+  for (let i = 0; i < list.length; i++) {
+    let newHtml = ``;
+    for (const j in list[i]) {
+      newHtml += `<td>${list[i][j]}</td>`;
+    }
+
+    html += `<tr>${newHtml} <td><button class="del-btn"><i class="fa-solid fa-trash trash-icon"></i></button></td></tr>`;
+  }
+  emplyeeTable.innerHTML = html;
   removeExistTable();
   allTables[3].classList.add("active__table");
 });
-customTableBtn.addEventListener("click", () => {
+customTableBtn.addEventListener("click", async () => {
   removeExistTable();
   allTables[4].classList.add("active__table");
 });
