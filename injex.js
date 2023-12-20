4e12d83939483e026228b9689d9bfd8d779831e1
EslamElkfafy <106202753+EslamElkfafy@users.noreply.github.com>
2023-12-20 21:00:25 +0200

    version 1.0.1

---
+++ injex.js
@@ -0,0 +1,43 @@
+import sql from 'mssql/msnodesqlv8.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const port = 8000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const pool = new sql.ConnectionPool({
+  database: 'master',
+  server: 'DESKTOP-UT15SN3\\MSSQLSERVER01',
+  driver: 'msnodesqlv8',
+  options: {
+    trustedConnection: true
+  }
+})
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/result", (req, res) => {
+  console.log(req.body);
+  pool.connect().then(() => {
+    //simple query
+    pool.request().query(`select * from ${req.body.fetch}`, (err, result) => {
+          console.dir(result)
+          let list_result = result["recordset"];
+          res.json({
+            keys: Object.keys(list_result[0]),
+            list : list_result
+          });
+      })
+  })
+  
+})
+
+app.listen(port, (req, res) => {
+  console.log(`server is running on port ${port}`);
+})
